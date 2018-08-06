854b56fab1651b3e31512d596ba59958585554aa
gabrielpiovanisantos <gabriel.santos@dcomp.sor.ufscar.br>
2018-08-05 21:17:06 -0300

    05/08

--- limiar.m
+++ limiar.m
@@ -1,9 +1,9 @@
-function[B]= limiar(A,darkest)
+function[B]= limiar(A,min, max)
 [lin, col] = size(A);
 B=A;
 for i = 1:lin
     for j=1:col
-        if A(i,j) > darkest && A(i,j) < 255 
+        if A(i,j) > 55 & A(i,j) < 100 
             B(i,j) = 255;
         else
             B(i,j) = 0;
--- projeto.m
+++ projeto.m
@@ -4,42 +4,46 @@
 close all
 clear all
 clc;
-[Im,map] = imread('imagem10.jpg');
-[ImInd,map] = rgb2ind(Im,255);
+[Im,map] = imread('imagem3.jpg');
+%[ImInd,map] = rgb2ind(Im,255);
 figure, imshow(Im),title('original');
+% [ImSeg] = seg_channel(Im);
+% figure, imshow(ImSeg),title('original');
+
 % Im2 = imfill(ImBW);
 % figure,imshow(Im2),title('imfill');
- Im = rgb2gray(Im);
+ImGray = rgb2gray(Im);
 % Darkest= min(ImGray(:));
-% se = strel('disk',10);
-% Im = imclose(ImGray,se);
-%  figure, imshow(Im),title('com imclose');
-% [lin, col] = size(Im);
-% C = round([col lin]/2);
+se = strel('disk',10);
+Im4 = imclose(ImGray,se);
+figure, imshow(Im4),title('com imclose');
 % Im = funcaoMediana(Im);
-% figure,imshow(Im),title('centro');
-% hold on
-% plot(C(1),C(2),'*r');
-% hold off
+figure,imshow(Im),title('centro');
+hold on
+[lin, col] = size(Im);
+C = round([col lin]/2);
+plot(C(1),C(2),'*r');
+hold off
  
 % % Im = uint8(Im);
 % % rgbIm = ind2rgb(Im,map);
 % % figure,imshow(rgbIm),title('rgb');
  
-Im = imbinarize(Im);
- 
+ Im = limiar(Im4);
+ RGB = label2rgb(bwlabel(Im),@jet,[.5 .5 .5]);
+ figure,imshow(RGB),title('centroids');
+
 % % rgbImage = ind2rgb(Im, map);
 % Im = imbinarize(Im);
 % % Im2 = imfill(Im);
+ propes = regionprops(Im,'Centroid');
+% whos;
+centroids = cat(1, propes.Centroid);
  figure,imshow(Im),title('centroids');
-% propes = regionprops(Im,'Centroid');
-% % whos;
-% centroids = cat(1, propes.Centroid);
-% figure,imshow(Im),title('centroids');
-% hold on
-% plot(centroids(:,1),centroids(:,2), 'b*')
-% hold off
-% [centers,radii] = imfindcircles(Im,[15 30]);
+hold on
+plot(centroids(:,1),centroids(:,2), 'b*')
+hold off
+% [centers,radii] = imfindcircles(Im,5);
 % figure,imshow(Im),title('circulos');
 % hold on
 % viscircles(centers, radii,'Color','b');
